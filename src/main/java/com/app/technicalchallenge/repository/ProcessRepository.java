61cd9dab99839af0548f9df0268c6c5a812669e9
MonacoBz <gerardossj2@hotmail.com>
2026-04-14 21:16:04 -0600

    feat(ProcessRepository): add repository of Processes

---
+++ src/main/java/com/app/technicalchallenge/repository/ProcessRepository.java
@@ -0,0 +1,6 @@
+package com.app.technicalchallenge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcessRepository extends JpaRepository<Process,Long> {
+}
--- src/main/java/com/app/technicalchallenge/service/ProcessService.java
+++ src/main/java/com/app/technicalchallenge/service/ProcessService.java
@@ -1,4 +1,7 @@
 package com.app.technicalchallenge.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProcessService {
 }
