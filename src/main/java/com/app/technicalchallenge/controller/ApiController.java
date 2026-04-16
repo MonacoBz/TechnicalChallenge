4a018978ad4ea8c9e6b098981ca17cb17676b387
MonacoBz <gerardossj2@hotmail.com>
2026-04-15 19:47:40 -0600

    feat(ApiController): add logic to test the creation of processes

--- src/main/java/com/app/technicalchallenge/controller/ApiController.java
+++ src/main/java/com/app/technicalchallenge/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.app.technicalchallenge.controller;
 
 import com.app.technicalchallenge.dto.ProcessResponseDto;
+import com.app.technicalchallenge.service.ProcessService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/process")
 public class ApiController {
 
+    private final ProcessService service;
+
+    public ApiController(ProcessService service){
+        this.service = service;
+    }
+
     @PostMapping("/start")
     public ResponseEntity<ProcessResponseDto> startProcess() {
-        return null;
+        return ResponseEntity.ok(service.createProcess());
     }
+
 }
