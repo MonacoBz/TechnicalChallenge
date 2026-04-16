a7c65f9a34a60894a4840862855a0801c45f1c0f
MonacoBz <gerardossj2@hotmail.com>
2026-04-15 19:39:41 -0600

    feat(Process): add UUID for a process

--- src/main/java/com/app/technicalchallenge/entities/Process.java
+++ src/main/java/com/app/technicalchallenge/entities/Process.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "processes")
 @AllArgsConstructor
@@ -17,6 +19,8 @@ public class Process {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private UUID uuid;
 
     private Status status;
 
