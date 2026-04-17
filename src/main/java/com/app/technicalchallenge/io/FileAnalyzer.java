00b586256f47c099312ad9ea01772091f5d2d9a8
MonacoBz <gerardossj2@hotmail.com>
2026-04-17 14:07:45 -0600

    fix(fileanalyzer): fix the race condition in this class

--- src/main/java/com/app/technicalchallenge/io/FileAnalyzer.java
+++ src/main/java/com/app/technicalchallenge/io/FileAnalyzer.java
@@ -14,46 +14,45 @@ import java.util.Set;
 
 public class FileAnalyzer {
 
-    private long totalLines = 0;
-    private long totalWords = 0;
-    Map<String,Integer> frequence;
-
-    public FileAnalyzer(){
-        frequence = new HashMap<>();
-    }
     public void analyze(Process procees, Resource resource){
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))){
-            while(reader.ready())prepareResults(reader.readLine(),procees);
-            setResults(procees,resource);
+            var resultAnalyzer = new ResultAnalyzer();
+            while(reader.ready())prepareResults(reader.readLine(),resultAnalyzer);
+            setResults(procees,resource,resultAnalyzer);
         }catch (Exception e){}
     }
 
-    private void prepareResults(String line,Process process){
-        var result = process.getResults();
-        totalLines++;
+    private void prepareResults(String line,ResultAnalyzer resultAnalyzer){
+        resultAnalyzer.totalLines++;
         String [] words = line.split(" ");
-        totalWords += words.length;
-        analyzeFrequenceWords(words);
+        resultAnalyzer.totalWords += words.length;
+        analyzeFrequenceWords(words,resultAnalyzer.frequence);
     }
 
-    private void analyzeFrequenceWords(String [] words){
+    private void analyzeFrequenceWords(String [] words,Map<String,Integer> frequence){
         for(String word : words)frequence.put(word,frequence.containsKey(word)? frequence.get(word) + 1 : 1);
     }
 
-    private void setResults(Process process, Resource resource){
-        int minFrequence = 30;
+    private void setResults(Process process, Resource resource,ResultAnalyzer resultAnalyzer){
+        int minFrequence = 50;
         Set<String> frequentWords = process.getResults().getMostFrequentWords();
-        for(String key : frequence.keySet()){
+        for(String key : resultAnalyzer.frequence.keySet()){
             if(!frequentWords.contains(key)){
-                var value = frequence.get(key);
+                var value = resultAnalyzer.frequence.get(key);
                 if(value >= 100)frequentWords.add(key);
             }
         }
         var result = process.getResults();
-        result.setTotalLines(totalLines);
+        result.setTotalLines(result.getTotalLines() + resultAnalyzer.totalLines);
         result.setMostFrequentWords(frequentWords);
-        result.setTotalWords(totalWords);
+        result.setTotalWords(result.getTotalWords() + resultAnalyzer.totalWords);
         result.getFileProccesed().add(resource.getFilename());
     }
 
+}
+
+class ResultAnalyzer{
+    Map<String,Integer> frequence = new HashMap<>();
+    long totalLines;
+    long totalWords;
 }
